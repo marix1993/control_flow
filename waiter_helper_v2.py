c3120da81c5bc5c142430df14c3a492fc37014a3
Mateusz Kardasz <kardaszmat@gmail.com>
2023-04-05 10:05:45 +0100

    Waiter helper v2 changes

--- waiter_helper_v2.py
+++ waiter_helper_v2.py
@@ -15,28 +15,29 @@ order_list = []
 ordering = True
 
 while ordering:
+    # Ordering starter:
     starters_order = input("What starter from the menu would you like?")
     if starters_order in starters:
         order_list.append(starters_order)
-        print(f"{order_list} added")
+        print(f"{order_list[0]} added")
     else:
         print("Choose something from the menu")
         continue
-    # Ordering main
+    # Ordering main:
     while ordering:
         main_order = input("What main from the menu would you like?")
         if main_order in mains:
             order_list.append(main_order)
-            print(f"{order_list} added")
+            print(f"{order_list[1]} added")
         else:
             print("Choose something from the menu")
             continue
-        # Ordering dessert
+        # Ordering dessert:
         while ordering:
             dessert_order = input("What dessert from the menu would you like?")
             if dessert_order in dessert:
                 order_list.append(dessert_order)
-                print(f"{order_list} added")
+                print(f"{order_list[2]} added")
                 ordering = False
             else:
                 print("Choose something from the menu")
